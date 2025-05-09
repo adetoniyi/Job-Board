8cb28b3104f0fa0ca9d93e10c8fe3aca962aea63
adetoniyi <adetoniyi04@gmail.com>
2025-05-10 00:41:32 +0100

    jonBoard

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+type student = {
+    name: string;
+    age: number;
+    gender: string;
+    score: number
+}
+let studenta: student = {
+    name: "Pelumi",
+    age: 27,
+    gender: "male",
+    score: 90   
+}
+console.log(studenta)
